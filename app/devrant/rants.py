2a882b793f93cbf44babec8d0f51b924f2cbff55
Evan Pratten <ewpratten@gmail.com>
2019-04-09 09:53:54 -0400

    error messages

--- app/devrant/rants.py
+++ app/devrant/rants.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint
-from flask import jsonify
+from output import respond
 from .external import *
 
 devrant_rants = Blueprint("devrant rants", __name__)
@@ -10,16 +10,19 @@ def info():
     id_diff = ids[0][0] - ids[1][0]
     time_diff = ids[0][1] - ids[1][1]
 
-    return jsonify({"success": True, "newest_id": ids[0][0], "newest_time": ids[0][1], "id_gap": id_diff, "time_gap": time_diff})
+    return respond({"success": True, "newest_id": ids[0][0], "newest_time": ids[0][1], "id_gap": id_diff, "time_gap": time_diff})
 
 @devrant_rants.route("/devrant/rants/<id>/people")
 def tba(id):
     rant = getRant(id)
 
-    users = [rant["rant"]["user_username"]]
+    try:
+        users = [rant["rant"]["user_username"]]
+    except:
+        return respond({"success": False, "error":"Rant does not exsist"})
 
     for comment in rant["comments"]:
         users.append(comment["user_username"])
 
-    return jsonify({"success": True, "users":users})
+    return respond({"success": True, "users":users})
     
