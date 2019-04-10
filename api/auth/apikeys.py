8330979b5a937c1f4162fb5af32f8d46c7b26a71
Evan Pratten <ewpratten@gmail.com>
2019-04-10 12:22:07 -0400

    added 5k24 key

--- api/auth/apikeys.py
+++ api/auth/apikeys.py
@@ -2,8 +2,14 @@
 all_scopes = ["tba", "frc", "devrant", "github", "rss", "rtl", "rtl-access"]
 
 read = {
+    # debugging key
     "070f42125103c1b1edd465a102c052c245d1eb455a35c370614c037c83e2c65c9646c699e8fa27184bc8dcf74f14e23c8a01cf9160b7718b82a8645261b6458d": ["tba", "rtl-access", "devrant"],
-    "cac386b5ca59ab4d6256f9ce548cd96af41bda212bc688c9ca710a5cec77fd078bbc3a5b0efb6232b7e705af4bb2bf2f412441c6610e4d2ca8b2af8451d5e69d": ["devrant"]
+
+    # devrant status key retrylife.ca
+    "cac386b5ca59ab4d6256f9ce548cd96af41bda212bc688c9ca710a5cec77fd078bbc3a5b0efb6232b7e705af4bb2bf2f412441c6610e4d2ca8b2af8451d5e69d": ["devrant"],
+
+    # 5k24 live data key
+    "8107b599263a5466f28fa6dc4d29a93feff43edcb2aa54761a7e3055fb57cb582b85a1cd403b5769f62fe38c593286994e9f121785ab36cc304db0f1255220a3": ["tba"]
 }
 
 internal_keys = {
